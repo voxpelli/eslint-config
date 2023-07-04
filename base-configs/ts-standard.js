df1860999a6b804870ffa11e2339b9818b22900a
Pelle Wessman <pelle@kodfabrik.se>
2023-07-04 15:24:00 +0200

    refactor: improve types

--- base-configs/ts-standard.js
+++ base-configs/ts-standard.js
@@ -2,19 +2,28 @@
 // which makes use of all extended rules https://typescript-eslint.io/rules/#extension-rules
 // Similar to https://github.com/typescript-eslint/typescript-eslint/blob/f4016c24f9023e8a42def9501b68c4a908cbfede/packages/eslint-plugin/src/configs/recommended.ts
 
-const standard = require('./base-configs/standard');
+const { rules: standardRules } = require('./standard');
 const { rules } = require('@typescript-eslint/eslint-plugin');
 
+/** @type {import('eslint').Linter.RulesRecord} */
 const replacedRules = {};
+/** @type {import('eslint').Linter.RulesRecord} */
 const replacementRules = {};
 
-for (const ruleId in rules) {
-  if (standard.rules[ruleId] && rules[ruleId].meta.docs.extendsBaseRule) {
+for (const tsRuleId in rules) {
+  const ruleId = /** @type {(keyof typeof standardRules) | undefined} */ (
+    tsRuleId in standardRules
+      ? tsRuleId
+      : undefined
+  );
+
+  if (ruleId && rules[ruleId]?.meta?.docs?.extendsBaseRule) {
     replacedRules[ruleId] = 'off';
-    replacementRules['@typescript-eslint/' + ruleId] = standard.rules[ruleId];
+    replacementRules['@typescript-eslint/' + ruleId] = standardRules[ruleId];
   }
 }
 
+/** @satisfies {import('eslint').ESLint.ConfigData} */
 const config = {
   'extends': ['./standard'],
   parser: '@typescript-eslint/parser',
