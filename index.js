fb7ce84e2b2f51931e979951328cd7847e3ea195
Pelle Wessman <pelle@kodfabrik.se>
2024-05-26 15:38:47 +0200

    feat: add back mocha rules

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import neostandard from 'neostandard';
 import { additionalRules } from './base-configs/additional-rules.js';
 import { esmRules } from './base-configs/esm.js';
 import { jsdocRules } from './base-configs/jsdoc.js';
+import { mochaRules } from './base-configs/mocha.js';
 
 /**
- * @param {{ cjs?: boolean }} [options]
+ * @param {{ cjs?: boolean, noMocha?: boolean }} [options]
  * @returns {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray}
  */
-export function voxpelli ({ cjs } = {}) {
+export function voxpelli ({ cjs, noMocha } = {}) {
   return [
     ...neostandard({ semi: true }),
     ...additionalRules,
     ...jsdocRules,
     ...cjs ? [] : esmRules,
+    ...noMocha ? [] : mochaRules,
   ];
 }
 
