c5a4840523c192c019ce6616cea7fb2449581deb
Pelle Wessman <pelle@kodfabrik.se>
2024-06-07 14:03:08 +0200

    fix: improved mocha config

--- base-configs/mocha.js
+++ base-configs/mocha.js
@@ -1,18 +1,18 @@
 // @ts-ignore
 import mochaPlugin from 'eslint-plugin-mocha';
-import globals from 'globals';
 import { config } from 'typescript-eslint';
 
 export const mochaRules = config({
   ignores: ['**/*', '!test/**/*'],
   'extends': [
     mochaPlugin.configs.flat.recommended,
-  ],
-  languageOptions: {
-    globals: {
-      ...globals.mocha,
+    {
+      files: ['**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-unused-expressions': 'off',
+      },
     },
-  },
+  ],
   rules: {
     'mocha/no-mocha-arrows': 'off',
     'no-unused-expressions': 'off',
