a07f6cbce54f0416ae4113c7dca2a918da036c93
Pelle Wessman <pelle@kodfabrik.se>
2024-07-01 16:48:45 +0200

    fix: revert avoid potential clash with neostandard `n/`
    This reverts commit 4e70c1a924e1d9fa4a5ac96459cbacf090b0374e.

--- base-configs/node.js
+++ base-configs/node.js
@@ -1,3 +1,4 @@
+// @ts-ignore
 import nPlugin from 'eslint-plugin-n';
 
 /**
@@ -7,12 +8,12 @@ import nPlugin from 'eslint-plugin-n';
 export function nodeRules (cjs) {
   return [
     {
-      ...renamePlugin('n', 'voxpelli-n', nPlugin.configs['flat/recommended-module']),
+      ...nPlugin.configs['flat/recommended-module'],
       // If CommonJS, only target *.mjs, target everything but *.cjs
       ...cjs ? { files: ['**/*.mjs'] } : { ignores: ['**/*.cjs'] },
     },
     {
-      ...renamePlugin('n', 'voxpelli-n', nPlugin.configs['flat/recommended-script']),
+      ...nPlugin.configs['flat/recommended-script'],
       // If CommonJS, target everything but *.mjs, else only target *.cjs
       ...cjs ? { ignores: ['**/*.mjs'] } : { files: ['**/*.cjs'] },
     },
@@ -20,13 +21,13 @@ export function nodeRules (cjs) {
       name: '@voxpelli/additional/node',
       rules: {
         // Overriding
-        'voxpelli-n/no-process-exit': 'off',
+        'n/no-process-exit': 'off',
 
         // Adding
-        'voxpelli-n/prefer-global/console': 'warn',
-        'voxpelli-n/prefer-promises/fs': 'warn',
-        'voxpelli-n/no-process-env': 'warn',
-        'voxpelli-n/no-sync': 'error',
+        'n/prefer-global/console': 'warn',
+        'n/prefer-promises/fs': 'warn',
+        'n/no-process-env': 'warn',
+        'n/no-sync': 'error',
       },
     },
     {
@@ -34,40 +35,8 @@ export function nodeRules (cjs) {
       files: ['**/*.ts'],
       rules: {
         // TODO: Remove when *.js files can be properly resolved from *.d.ts
-        'voxpelli-n/no-missing-import': 'off',
+        'n/no-missing-import': 'off',
       },
     },
   ];
-}
-
-/**
- * @param {string} existingPluginName
- * @param {string} newPluginName
- * @param {import('eslint').Linter.FlatConfig} rawConfig
- * @returns {import('eslint').Linter.FlatConfig}
- */
-function renamePlugin (existingPluginName, newPluginName, rawConfig) {
-  const config = {
-    ...rawConfig,
-    plugins: { ...rawConfig.plugins },
-    rules: { ...rawConfig.rules },
-  };
-
-  if ('plugins' in config && 'rules' in config) {
-    const plugin = config.plugins[existingPluginName];
-
-    if (plugin) {
-      config.plugins[newPluginName] = plugin;
-      delete config.plugins[existingPluginName];
-
-      for (const key in config.rules) {
-        config.rules[key.replaceAll(existingPluginName + '/', newPluginName + '/')] = config.rules[key];
-        delete config.rules[key];
-      }
-
-      return config;
-    }
-  }
-
-  throw new Error(`Could not rename "${existingPluginName}" plugin`);
 }
